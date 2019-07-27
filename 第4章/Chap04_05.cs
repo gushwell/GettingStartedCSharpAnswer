ebbadd3878f156f3b14157139a3ae95f37cd140a
gushwell <gushwell@gmail.com>
2019-07-27 13:26:04 +0900

    first commit

---
+++ 第4章/Chap04_05.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Chap04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var line = Console.ReadLine();
+            var num = int.Parse(line);
+            if (0 <= num && num <= 30)
+            {
+                Console.WriteLine("不可");
+            }
+            else if (num <= 60)
+            {
+                Console.WriteLine("可");
+            }
+            else if (num <= 80)
+            {
+                Console.WriteLine("良");
+            }
+            else if (num <= 100)
+            {
+                Console.WriteLine("優");
+            }
+            else
+            {
+                Console.WriteLine("入力した数値に誤りがあります");
+            }
+        }
+    }
+}
---
+++ 第5章/Chap05_01.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Chap05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var line = Console.ReadLine();
+                var n = int.Parse(line);
+                if (n < 0)
+                {
+                    break;
+                }    
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
