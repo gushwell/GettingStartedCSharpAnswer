11b86afea86451b61070e1108962377da3aaeda4
gushwell <gushwell@gmail.com>
2020-02-05 21:20:42 +0900

    Update Chap04_05.cs

--- 第4章/Chap04_05.cs
+++ 第4章/Chap04_05.cs
@@ -1,21 +1,32 @@
 using System;
 
-namespace Chap04 {
-    class Program {
-        static void Main(string[] args) {
+namespace Chap04 
+{
+    class Program 
+    {
+        static void Main(string[] args) 
+        {
             var line = Console.ReadLine();
             var num = int.Parse(line);
-            if (num < 0 || 100 < num) {
+            if (num < 0 || 100 < num) 
+            {
                 Console.WriteLine("入力した数値に誤りがあります");
                 return;
             }
-            if (num <= 30) {
+            if (num <= 30) 
+            {
                 Console.WriteLine("不可");
-            } else if (num <= 60) {
+            } 
+            else if (num <= 60) 
+            {
                 Console.WriteLine("可");
-            } else if (num <= 80) {
+            } 
+            else if (num <= 80) 
+            {
                 Console.WriteLine("良");
-            } else if (num <= 100) {
+            } 
+            else if (num <= 100) 
+            {
                 Console.WriteLine("優");
             }
         }
